e6741bfc04a298d19d25448ff99894dbb06ec6fc
vdmkrchkn <vdmkrchkn@gmail.com>
2015-01-05 02:21:22 +0300

    Добавлен класс нейрона

---
+++ neuron.cpp
@@ -0,0 +1,58 @@
+#include "neuron.h"
+
+Neuron::Neuron() : threshold(0){}
+
+Neuron::~Neuron(){}
+
+int Neuron::thresholdFunc() const
+{
+    return this->value >= this->threshold ? 1 : 0;
+}
+
+void Neuron::setValue(int val)
+{
+    this->value = val;
+}
+
+void Neuron::addValue()
+{
+    this->addValue(1);
+}
+
+void Neuron::addValue(int n)
+{
+    this->value += n;
+}
+
+int Neuron::getValue() const
+{
+    return this->value;
+}
+
+QList<int> Neuron::getLinks() const
+{
+    return this->links;
+}
+
+
+Sensor::Sensor(int edges) : Neuron()
+{
+    for(int i = 0; i < edges; ++i)
+    // инициализация весов S-A в интервале [-1,1]
+        this->links << qrand() % 3 - 1;
+}
+
+
+Associative::Associative(int edges) : Neuron()
+{
+    // задание случайного порога
+    this->threshold = qrand() % 3 - 1;
+    for (int i = 0; i < edges; ++i)
+    // инициализация весов A-R 0-значениями
+        this->links << 0;
+}
+
+void Associative::setLinkWeight(int idx, int weight)
+{
+    this->links[idx] = weight;
+}
---
+++ neuron.h
@@ -0,0 +1,47 @@
+#ifndef NEURON_H
+#define NEURON_H
+
+#include <QList>
+
+class Neuron
+{
+public:    
+    virtual ~Neuron();
+    //  Пороговая функция
+    virtual int thresholdFunc() const;
+    //
+    virtual void setValue(int val);
+    // увеличение value на 1
+    virtual void addValue();
+    // увеличение value на n
+    virtual void addValue(int n);
+    // геттер для value
+    virtual int getValue() const;
+    // геттер для links
+    virtual QList<int> getLinks() const;
+protected:
+    Neuron();
+    // значение нейрона
+    int value;
+    // порог для передаточной ф-и
+    int threshold;
+    // веса синапсов
+    QList<int> links;
+};
+
+class Sensor : public Neuron
+{
+public:
+    Sensor(int edges);
+};
+
+class Associative : public Neuron
+{
+public:
+    Associative(int edges);
+    void setLinkWeight(int idx, int weight);
+};
+
+class Resulting : public Neuron{};
+
+#endif // NEURON_H
